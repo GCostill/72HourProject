b7a566d8016cc5e8bfcae927317d747622d95a03
GCostill <gaby123le@gmail.com>
2021-08-24 20:09:05 -0400

    Add project files.

---
+++ 72HourProject/Models/POCOs/Reply.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace _72HourProject.Models.POCOs
+{
+    public class Reply
+    {
+        public int Id { get; set; }
+        [ForeignKey(nameof(Comment))]
+        public int CommentId { get; set; }
+        public virtual Comment Comment { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+    }
+}
