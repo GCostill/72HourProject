12259d46140aed0f98ed1835a97f419e2ba80a12
GCostill <gaby123le@gmail.com>
2021-08-26 19:13:40 -0400

    AddedToCommentData

--- 72HourProject/Data/CommentData.cs
+++ 72HourProject/Data/CommentData.cs
@@ -7,5 +7,12 @@ namespace _72HourProject.Data
 {
     public class CommentData
     {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public Guid AuthorId { get; set; }
+        public virtual List<Reply> Replies { get; set; }
+        [ForeignKey(nameof(Post))]
+        public int PostId { get; set; }
+        public virtual Post Post { get; set; }
     }
 }
