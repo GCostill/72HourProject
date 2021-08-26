853f3825713b969d276b59965b1379feb42d9dc5
GCostill <gaby123le@gmail.com>
2021-08-26 19:19:33 -0400

    FinalizedCommentDataAndCommentModel

--- 72HourProject/Data/CommentData.cs
+++ 72HourProject/Data/CommentData.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using _72HourProject.Models.POCOs;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
